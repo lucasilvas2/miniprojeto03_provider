78a386f85760a753ba50b6ddfddc02ce3c80d63e
lucasilvas2 <lucasoliver1068@gmail.com>
2022-05-29 22:20:14 -0300

    update git

--- lib/screens/favoritos_screen.dart
+++ lib/screens/favoritos_screen.dart
@@ -7,6 +7,8 @@ import 'package:provider/provider.dart';
 class FavoritosScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<PlaceList>(context);
+    final List<Place> loadedPlaces = provider.getFavoritePlace;
     return Center(
       child: Column(
         children: [
@@ -14,20 +16,31 @@ class FavoritosScreen extends StatelessWidget {
             'Meus Lugares Favoritos',
             style: Theme.of(context).textTheme.headline4,
           ),
+          // Expanded(
+          //   child: Consumer<PlaceList>(
+          //     builder: (context, value, child) {
+          //       //List<Place> favorite = value.getListFavoritePlace;
+          //       return ListView.builder(
+          //           itemCount: value.getListPlace.length,
+          //           itemBuilder: (ctx, index) {
+          //             return value.getListPlace.isEmpty
+          //                 ? Text('Lista de favotiros vazia')
+          //                 : PlaceItem(value.getListPlace[index]);
+          //           });
+          //     },
+          //   ),
+          // ),
           Expanded(
-            child: Consumer<PlaceList>(
-              builder: (context, value, child) {
-                //List<Place> favorite = value.getListFavoritePlace;
-                return ListView.builder(
-                    itemCount: value.getListPlace.length,
-                    itemBuilder: (ctx, index) {
-                      return value.getListPlace.isEmpty
-                          ? Text('Lista de favotiros vazia')
-                          : PlaceItem(value.getListPlace[index]);
-                    });
-              },
+            child: Container(
+              child: ListView.builder(
+                  itemCount: loadedPlaces.length,
+                  itemBuilder: (ctx, index) {
+                    return loadedPlaces.isEmpty
+                        ? Text('Lista de favotiros vazia')
+                        : PlaceItem(loadedPlaces[index]);
+                  }),
             ),
-          )
+          ),
         ],
       ),
     );
